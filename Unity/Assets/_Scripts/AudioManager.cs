c0c3392dd3a0bc8cdd78f4c10fe146ba68512d69
yrojjory <me@jory.org>
2016-01-29 22:11:19 +0100

    Switched project to Forced Text. Started implementing Audio engine

---
+++ Unity/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AudioManager
+{
+
+	private static void playAudio(string eventName)
+	{
+		//AUDIO: without position
+		Fabric.EventManager.Instance.PostEvent(eventName);
+	}
+
+	private static void playAudioWithPosition(string eventName, GameObject ob)
+	{	
+		//AUDIO: with position
+		Fabric.EventManager.Instance.PostEvent(eventName, ob);
+	}
+
+	public static bool FabricLoaded {get { return Fabric.EventManager.Instance; }}
+
+
+	public static void PlaySound(string n)
+	{
+		LoadFabric();
+		if (FabricLoaded)
+			playAudio(n);
+	}
+
+	public static void PlaySound(string n, GameObject ob)
+	{
+		LoadFabric();
+		if (FabricLoaded)
+			playAudioWithPosition(n, ob);
+	}
+
+	public static void StopSound(string n)
+	{
+		Fabric.EventManager.Instance.PostEvent(n, Fabric.EventAction.StopAll);
+	}
+
+	public static void FadeOutMusic(string n) {
+		// fade out the music!
+		Fabric.Component component = Fabric.FabricManager.Instance.GetComponentByName(n);
+		if (component != null) {
+			component.FadeOut(0.1f, 0.5f);
+		}
+    }
+
+	public static void SetDialogLine(string dialogEvent, string componentName)
+	{
+		Fabric.EventManager.Instance.PostEvent(componentName, Fabric.EventAction.SetAudioClipReference, dialogEvent);
+	}
+
+	public static void LoadFabric()
+	{
+		if (FabricLoaded) { // || Application.isLoadingLevel) {
+			return;
+		}
+		Application.LoadLevelAdditive("Audio");
+	}
+}
