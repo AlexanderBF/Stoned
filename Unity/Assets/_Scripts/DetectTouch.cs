1936436e36d1b3023b361e4faabcac47cb6d3ef6
Elkbender <ingebwang@gmail.com>
2016-01-30 03:27:22 +0100

    Merged all functions, nothing works

--- Unity/Assets/_Scripts/DetectTouch.cs
+++ Unity/Assets/_Scripts/DetectTouch.cs
@@ -12,30 +12,35 @@ public class DetectTouch : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //detect touches
-		if (Input.touchCount > 0)
-        {
-            for (int i = 0; i < Input.touchCount; i++)
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(i).position), out hit))
-                {
-                    //rotating inner circle
-                    if (hit.collider == innerCircle)
-                    {
-                        Rotate(innerCircle, Vector3.forward);
-                    }
+		if (Input.touchCount > 0) {
+			for (int i = 0; i < Input.touchCount; i++) {
+				RaycastHit hit;
+				if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.GetTouch (i).position), out hit)) {
+					//rotating inner circle
+					if (hit.collider == innerCircle) {
+						Rotate (innerCircle, Vector3.forward);
+					}
                     //rotating outer circle
-                    else if (hit.collider == outerCircle)
-                    {
-                        Rotate(outerCircle, Vector3.back);
-                    }
-                }
-            }
-			
+                    else if (hit.collider == outerCircle) {
+						Rotate (outerCircle, Vector3.back);
+					}
+				}
+			}
+		}
+		if (Input.touchCount > 0) {
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out hit)) {
+				if(hit.collider == innerCircle) {
+					Rotate(innerCircle, Vector3.down);
+				} else if (hit.collider == outerCircle) {
+					Rotate(outerCircle, Vector3.up);
+				}
+			}
 		}
 	}
 
 	void Rotate(Collider circle, Vector3 direction) {
 		circle.transform.Rotate(direction);
+		Handheld.Vibrate ();
 	}
 }
