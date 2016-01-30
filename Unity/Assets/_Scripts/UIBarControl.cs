000be91042a1d636fb7c5b9c1bb2b23fcd0666d4
Åsmund Aqissiaq Arild Kløvstad <ASKL@TYH63086.agderskolen.no>
2016-01-30 14:56:47 +0100

    UI with particles (again)

--- Unity/Assets/_Scripts/UIBarControl.cs
+++ Unity/Assets/_Scripts/UIBarControl.cs
@@ -4,21 +4,40 @@ using System.Collections;
 public class UIBarControl : MonoBehaviour {
 
     public float fill;
-    public GameObject mask;
 
+    GameObject mask;
+    ParticleSystem particles;
     RectTransform maskTransform;
 
     void Start()
     {
         //get transform for the mask
+        mask = gameObject.transform.FindChild("Mask").gameObject;
         maskTransform = mask.GetComponent<RectTransform>();
+        particles = gameObject.GetComponentInChildren<ParticleSystem>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fill += 10;
+            StartCoroutine("UpdateBar", fill);
+        }
     }
 
     //public function for updating the bars
-    public void UpdateBar(float input)
+    public IEnumerator UpdateBar(float input)
     {
-        input = Mathf.Clamp(input, 0, 100);
-        maskTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, input);
+
+        while (maskTransform.rect.height < input)
+        {
+            particles.gameObject.transform.localPosition = new Vector3(0, input - 50, -1);
+            input = Mathf.Clamp(input, 0, 100);
+            maskTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Lerp(maskTransform.rect.height, input, Time.deltaTime * 30));
+            yield return null;
+        }
     }
 
     //function for powers
