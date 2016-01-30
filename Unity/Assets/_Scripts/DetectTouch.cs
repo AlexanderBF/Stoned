fe0d1445cac0c1c17240329854e214a729e6d729
Elkbender <ingebwang@gmail.com>
2016-01-30 04:15:49 +0100

    Fixed rotating discs and multitouch

--- Unity/Assets/_Scripts/DetectTouch.cs
+++ Unity/Assets/_Scripts/DetectTouch.cs
@@ -11,29 +11,16 @@ public class DetectTouch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //detect touches
+		//detect touches
 		if (Input.touchCount > 0) {
 			for (int i = 0; i < Input.touchCount; i++) {
 				RaycastHit hit;
-				if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.GetTouch (i).position), out hit)) {
-					//rotating inner circle
-					if (hit.collider == innerCircle) {
-						Rotate (innerCircle, Vector3.forward);
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(i).position), out hit)) {
+					if(hit.collider == innerCircle) {
+						Rotate(innerCircle, Vector3.down);
+					} else if (hit.collider == outerCircle) {
+						Rotate(outerCircle, Vector3.up);
 					}
-                    //rotating outer circle
-                    else if (hit.collider == outerCircle) {
-						Rotate (outerCircle, Vector3.back);
-					}
-				}
-			}
-		}
-		if (Input.touchCount > 0) {
-			RaycastHit hit;
-			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out hit)) {
-				if(hit.collider == innerCircle) {
-					Rotate(innerCircle, Vector3.down);
-				} else if (hit.collider == outerCircle) {
-					Rotate(outerCircle, Vector3.up);
 				}
 			}
 		}
