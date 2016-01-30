9ec6b77d290bdd943ae064a944ab3efe912e4352
unknown <ASKL@TYH63086.agderskolen.no>
2016-01-30 01:30:49 +0100

    Rudimentary UI

---
+++ Unity/Assets/UIBarControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIBarControl : MonoBehaviour {
+
+    public float fill;
+    public GameObject mask;
+
+    RectTransform maskTransform;
+
+    void Start()
+    {
+        maskTransform = mask.GetComponent<RectTransform>();
+    }
+
+
+    public void UpdateBar(float input)
+    {
+        input = Mathf.Clamp(input, 0, 100);
+        maskTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, input);
+    }
+}
