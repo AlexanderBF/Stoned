32fa4b22100e049c4ac8c0e894237ecaeb64afb7
yrojjory <me@jory.org>
2016-01-31 01:56:17 +0100

    Added Spirit Colection sound and trigger

---
+++ Unity/Assets/_Scripts/SoundSpiritCollectionTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSpiritCollectionTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+		Debug.Log("We have collected a spirit!");
+		AudioManager.PlaySound("FX/Gameplay/Spirits/Collected", this.gameObject);
+	}
+	
+}
