43ed30d9465ab3e97b3c02cabbcdde8f3208093e
Elkbender <ingebwang@gmail.com>
2016-01-31 02:30:42 +0100

    Fading added to TitleScreen

--- Unity/Assets/_Scripts/StartGame.cs
+++ Unity/Assets/_Scripts/StartGame.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 
 public class StartGame : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-			Application.LoadLevel ("Main Alex");
+			StartCoroutine(FadeToNextLevel());
 		}
+		if(Input.GetMouseButton(0)){
+			StartCoroutine(FadeToNextLevel());
+		}
+	}
+
+	IEnumerator FadeToNextLevel(){
+		float fadeTime = GameObject.Find ("Title Screen").GetComponent<Fader> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel ("Main Alex");
 	}
 }
